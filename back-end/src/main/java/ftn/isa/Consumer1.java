47c3b7558643ab9deda522a14193c3f4eed6de75
Bosko Kulusic <kulusicbosko@gmail.com>
2024-01-29 11:38:49 +0100

    consumer-producer using rabbit mq, external and main app can communicate from now on using text messages

---
+++ back-end/src/main/java/ftn/isa/Consumer1.java
@@ -0,0 +1,22 @@
+package ftn.isa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+@Component
+public class Consumer1 {
+
+    private static final Logger log = LoggerFactory.getLogger(Consumer1.class);
+    /*
+     * @RabbitListener anotira metode za kreiranje handlera za bilo koju poruku koja pristize,
+     * sto znaci da ce se kreirati listener koji je konektovan na RabbitQM queue i koji ce
+     * prosledjivati poruke metodi. Listener ce konvertovati poruku u odgovorajuci tip koristeci
+     * odgovarajuci konvertor poruka (implementacija org.springframework.amqp.support.converter.MessageConverter interfejsa).
+     */
+    @RabbitListener(queues="glavna")
+    public void handler(String message){
+        log.info("Dobio sam ovo> " + message);
+    }
+}
+
